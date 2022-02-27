0eaf8637f567ad31f3272653c33f189cb5d3b5a3
Ryan Deckard <ryan.e.deckard@outlook.com>
2022-02-27 10:45:49 -0600

    update

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-url="http://64.227.39.88:31815"
-
-for i in {1..10}; do
-        for link in $(curl -i -s -k -X POST -d 'uid=$i' "$url/documents.php?"; do
-                echo ${url}${link}
-        done
-done
